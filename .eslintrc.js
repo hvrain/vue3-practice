1fb6fb03af65c174469ce74f04da8c7c48bbb4f1
hvrain <hvrain3222@naver.com>
2022-04-10 19:47:36 +0900

    setting

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    node: true,
+  },
+  extends: [
+    //vue
+    "plugin:vue/vue3-strongly-recommended",
+    //js
+    "eslint:recommended"
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
+    sourceType: 'module',
+  },
+  rules: {
+    "vue/html-closing-bracket-newline": ["error", {
+      "singleline": "never",
+      "multiline": "never"
+    }],
+    "vue/html-self-closing": ["error", {
+      "html": {
+        "void": "always",
+        "normal": "never",
+        "component": "always"
+      }
+    }],
+    "vue/multi-word-component-names": ["error", {
+      "ignores": ["Fruit"]
+    }]
+  }
+};
